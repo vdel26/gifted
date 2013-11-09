1bddcb547f8362d634b84b7917c070eb2805fc08
Victor Delgado <vdel26@gmail.com>
2013-11-09 01:31:04 +0100

    Initial Commit

---
+++ TestGifted/GFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GFAppDelegate.h
+//  TestGifted
+//
+//  Created by Victor Delgado on 11/9/13.
+//  Copyright (c) 2013 Victor Delgado. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface GFAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ TestGifted/GFAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  GFAppDelegate.m
+//  TestGifted
+//
+//  Created by Victor Delgado on 11/9/13.
+//  Copyright (c) 2013 Victor Delgado. All rights reserved.
+//
+
+#import "GFAppDelegate.h"
+
+@implementation GFAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
